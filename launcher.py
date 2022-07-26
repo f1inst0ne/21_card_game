79d61e6c31b1342caca1374d60da3cc1ee8a586a
f1inst0ne <82869372+f1inst0ne@users.noreply.github.com>
2022-07-26 15:12:49 +0300

    Completed version for win
    Complited terminal Blackjack game, but realized, that some commands can`t work with linux. So in next commit it must be fixed.

---
+++ launcher.py
@@ -0,0 +1,16 @@
+import os
+os.system('title Blackjack')
+os.system('cls')
+flag = True
+while flag: 
+    print('Welcome to blackjack game')
+    print('What do you want to do?')
+    print('1)Play \n2)Exit')
+    user_input = input('>>>')
+    if user_input == '1':
+        os.system('python main.py')
+    elif user_input == '2':
+        flag = False
+    else:
+        print('invaild command!!!!!! Try again!')
+os.system('cls')
